63600ae6f41f378cd4299b5ecae3a282c91c0512
Mohammed Nouman <mohammednouman063@gmail.com>
2026-05-21 18:37:40 +0530

    fix:internal server error fixed

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://todofsd-4.onrender.com',
+        target: 'http://localhost:3000',
         changeOrigin: true,
       }
     }
